4793200e96488779fdc7a90fc3b392bdd0bba3b5
EgorVyhodcev <90092126+EgorVyhodcev@users.noreply.github.com>
2021-10-22 16:16:54 +0300

    fix ind2

--- LB7/ind2.py
+++ LB7/ind2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 if __name__ == '__main__':
     print("Enter the elements of the list a:")
     A = list(map(float, input().split()))
@@ -8,20 +9,14 @@ if __name__ == '__main__':
     print(length)
     print(f"The max element of this list is: {max(A)}")
     i = 0
-    for el in A:
+    for index, el in enumerate(A):
         if el >= 0:
-            i = A.index(el)
-    s = sum([a for a in A if A.index(a) < i])
+            i = index
+    s = sum([a for index, a in enumerate(A) if index < i])
     print(f"The sum of numbers before the last positive element is: {s}")
     a = int(input("Enter the a number of the border:"))
     b = int(input("Enter the b number of the border:"))
-    temp = []
-    for el in A:
-        if a < abs(el) < b:
-            continue
-        else:
-            temp.append(el)
-    if length > len(temp):
-        for i in range(length - len(temp)):
-            temp.append(0)
+    temp = [el for el in A if b < abs(el) or abs(el) < a]
+    x = length - len(temp)
+    temp.extend([0] * x)
     print(temp)
